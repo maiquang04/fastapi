e92c87109aed66b9a6ea15fcda342829f80e833c
Quang Mai <thanhnhatquang.mai@student.uq.edu.au>
2025-12-06 10:50:16 +0700

    Modify create_post to store created post

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -29,7 +30,9 @@ def get_posts():
     return {"data": my_posts}
 
 
-@app.post("/ posts")
-def create_posts(new_post: Post):
-    print(new_post.model_dump())
-    return {"message": "successfully created post"}
+@app.post("/posts")
+def create_post(post: Post):
+    post_dict = post.model_dump()
+    post_dict["id"] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
