d2a93fe1a2b866fde57cdf4cd68d882ae2e8a70a
Quang Mai <thanhnhatquang.mai@student.uq.edu.au>
2025-12-06 10:24:41 +0700

    Add validation to create posts and store posts temporarily on memory

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {"id": 1, "title": "title of post", "content": "content of post"},
+    {"id": 2, "title": "favorite food", "content": "braised pork"},
+]
 
 
 @app.get("/")
@@ -10,4 +26,10 @@ def read_root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
+
+
+@app.post("/ posts")
+def create_posts(new_post: Post):
+    print(new_post.model_dump())
+    return {"message": "successfully created post"}
