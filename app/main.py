753594d64dba395d2e13122e95d1f22920894008
Quang Mai <thanhnhatquang.mai@student.uq.edu.au>
2025-12-06 21:31:14 +0700

    Add api to update post

--- app/main.py
+++ app/main.py
@@ -65,9 +65,25 @@ def create_post(post: Post):
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
     index = find_post_index(id)
-    if index == None:
+    if index is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"post with id: {id} does not exist",
         )
     my_posts.pop(index)
+
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_post_index(id)
+
+    if index is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id: {id} does not exist",
+        )
+
+    post_dict = post.model_dump()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return {"data": post_dict}
